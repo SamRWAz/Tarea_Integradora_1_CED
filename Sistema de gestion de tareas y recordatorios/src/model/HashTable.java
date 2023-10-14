d5bd9033b08b3e5950d3f7ae8e3ba6cad1e895b0
unknown <09juanesteban@gmail.com>
2023-10-13 20:37:09 -0500

    Creacion de la clase 'HashTable' y implementacion de codigo de la tabla hash

---
+++ Sistema de gestion de tareas y recordatorios/src/model/HashTable.java
@@ -0,0 +1,65 @@
+package model;
+
+import java.util.LinkedList;
+
+public class HashTable<K, V> {
+    private LinkedList<Entry<K, V>>[] table;
+    private int capacity;
+    private int size;
+
+    public HashTable(int capacity) {
+        this.capacity = capacity;
+        table = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            table[i] = new LinkedList<Entry<K, V>>();
+        }
+    }
+
+    public void put(K key, V value) {
+        int index = hash(key);
+        for (Entry<K, V> entry : table[index]) {
+            if (entry.getKey().equals(key)) {
+                entry.setValue(value);
+                return;
+            }
+        }
+        table[index].add(new Entry<K, V>(key, value));
+        size++;
+    }
+
+    public V get(K key) {
+        int index = hash(key);
+        for (Entry<K, V> entry : table[index]) {
+            if (entry.getKey().equals(key)) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+    private int hash(K key) {
+        return Math.abs(key.hashCode() % capacity);
+    }
+
+    private static class Entry<K, V> {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+    }
+}
